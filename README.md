7d3909128e29013c9cd850b1436c47f7109ce278
emu47 <kkelly219@gmail.com>
2017-03-22 11:06:35 -0400

    [KK] More initial setup, plus a revised way of streaming the item records that is actually good. Also including a test / reduced items.ndjson file for development.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # sierra-items-s3-reader
 Reads items from Sierra export saved in S3 export file, and publishes SierraItem to the SierraItemPostRequest Stream
+
+#Setup
+First, you'll need access to the AWS streams, or will need to create your own test streams. This lambda requires:
+
+* Local items.ndjson file of NYPL Sierra catalog item records: This will be the source of the records the app will encode and pass to the Kinesis stream. (An example file with two records is included. Change the name to 'items.ndjson' to use it.)
+* A Kinesis stream to post records to. (Feel free to use 'testS3ItemReader' to test.)
+* A schema to use to validate avro conversion. Current lambda uses https://api.nypltech.org/api/v0.1/current-schemas/SierraItemRetrievalRequest
+
+To configure AWS appropriately, install the AWS SDK, then run 'aws confingure' and set your keys to the appropriate values. Get the credentials from the team.
+
+#What it Does
+This is a simple lambda that reads from a large local dump of files, encodes the streamed records into avro, and posts to a kinesis stream for more processing.
+
+Once you have the items.ndjson file at the root of the lambda, run the application with the standard command: 'node-lambda run'.
+
+#In the Future the lambda may want to think about if it wants to... 
+* Watch the directory proactively and run when a new ndjson file is added or a current file is updated, instead of requiring manual copying of files and manually running the lambda.
+* Possible accept records from other external sources other than the NYPL Sierra catalog.
--- index.js
+++ index.js
@@ -15,12 +15,72 @@ exports.handler = function(event, context){
   var getStream = function () {
       var jsonData = 'items.ndjson',
           stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
-          parser = JSONStream.parse('*');
+          parser = JSONStream.parse();
           return stream.pipe(parser);
   };
 
    getStream()
     .pipe(es.mapSync(function (data) {
-      console.log(data);
+      var recordString = JSON.stringify(data);
+      console.log(recordString);
+      kinesisHandler(recordString, context);
     }));
 };
+
+//kinesis stream handler
+var kinesisHandler = function(record, context) {
+  console.log('Processing ' + record );
+  var url = "https://api.nypltech.org/api/v0.1/current-schemas/SierraItemRetrievalRequest";
+  schema(url)
+    .then(function(schema_data) {
+      // var json_data = avro_decoded_data(schema_data, record);
+      console.log(record);
+      postItemsStream(record);
+    })
+    .catch(function(e){
+      console.log(e);
+    });
+}
+
+//get schema
+var request = require('request');
+var schema = function(url, context) {
+  return new Promise(function(resolve, reject) {
+    request(url, function(error, response, body) {
+      if(!error && response.statusCode == 200){
+        resolve(JSON.parse(body).data.schema);
+      }else{
+        console.log("An error occurred - " + response.statusCode);
+        reject("Error occurred while getting schema - " + response.statusCode);
+      }
+    })
+  })
+}
+
+//use avro to deserialize
+var avro_decoded_data = function(schema_data, record){
+  // const type = avro.infer(record.to_json);
+  // console.log(type);
+  // const item_in_avro_format = type.toBuffer(record);
+  // var decoded = new Buffer(record, 'base64');
+  // var verify = type.fromBuffer(decoded);
+  return record; // JSON.parse(item_in_avro_format);
+}
+
+//send data to kinesis Stream
+var postItemsStream = function(item){
+  const type = avro.infer(item);
+  console.log(type.getSchema());
+  const item_in_avro_format = type.toBuffer(item);
+  var params = {
+    Data: item_in_avro_format, /* required */
+    PartitionKey: crypto.randomBytes(20).toString('hex').toString(), /* required */
+    StreamName: 'testS3ItemReader', /* required */
+  }
+  kinesis.putRecord(params, function (err, data) {
+    if (err) console.log(err, err.stack) // an error occurred
+    else     console.log(data)           // successful response
+    //cb(null,data)
+  })
+
+}
